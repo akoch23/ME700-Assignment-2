c5cea787e2c6c1c0ef5ec7579f3203c0ae9685d7
akoch23 <akoch23@bu.edu>
2025-02-26 10:36:17 -0500

    Update DSM_functions.py

--- DSM/DSM_functions.py
+++ DSM/DSM_functions.py
@@ -1,22 +1,35 @@
 
-node(self, u, v, w, theta_x, theta_y, theta_z):
+node(self, u, v, w, theta_x, theta_y, theta_z, f_x, f_y, f_z, mu_x, mu_y, mu_z)):
     self.u = u
     self.v = v
     self.w = w
     self.theta_x = theta_x
     self.theta_y = theta_y
     self.theta_z = theta_z
+    self.f_x = f_x
+    self.f_y = f_y
+    self.f_z = f_z
+    self.mu_x = mu_x
+    self.mu_y = mu_y
+    self.mu_z = mu_z
     
-    def node_coords(self):
+    def node_dof(self):
         return [self.u, self.v, self.w, self.theta_x, self.theta_y, self.theta_z]
+
+    def node_loads(self):
+        return [self.f_x, self.f_y, self.f_z, self.mu_x, self.mu_y, self.mu_z]
     
 element(self, node_1, node_2):
     self.node_1 = node_1
     self.node_2 = node_2
     
     def displacement_rot_vector(self):
-        return [self.node_1.node_coords(), self.node_2.node_coords()]
+        return [self.node_1.node_dof(), self.node_2.node_dof()]
         
-    
-def stiffness_matrix ():
+    def load_vector(self):
+        return [self.node_1.node_loads(), self.node_2.node_loads()]
 
+def stiffness_matrix(F, k, E, A, L, u):
+    
+    
+    
