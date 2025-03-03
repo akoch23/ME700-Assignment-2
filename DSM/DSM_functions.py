2eca0b6a666f7bb3aa50cc9e3874522a1eab216f
akoch23 <akoch23@bu.edu>
2025-03-03 09:54:41 -0500

    Update DSM_functions.py

--- DSM/DSM_functions.py
+++ DSM/DSM_functions.py
@@ -1,35 +1,187 @@
+import numpy as np
 
-node(self, u, v, w, theta_x, theta_y, theta_z, f_x, f_y, f_z, mu_x, mu_y, mu_z)):
-    self.u = u
-    self.v = v
-    self.w = w
-    self.theta_x = theta_x
-    self.theta_y = theta_y
-    self.theta_z = theta_z
-    self.f_x = f_x
-    self.f_y = f_y
-    self.f_z = f_z
-    self.mu_x = mu_x
-    self.mu_y = mu_y
-    self.mu_z = mu_z
-    
-    def node_dof(self):
-        return [self.u, self.v, self.w, self.theta_x, self.theta_y, self.theta_z]
 
-    def node_loads(self):
-        return [self.f_x, self.f_y, self.f_z, self.mu_x, self.mu_y, self.mu_z]
-    
-element(self, node_1, node_2):
-    self.node_1 = node_1
-    self.node_2 = node_2
-    
-    def displacement_rot_vector(self):
-        return [self.node_1.node_dof(), self.node_2.node_dof()]
-        
-    def load_vector(self):
-        return [self.node_1.node_loads(), self.node_2.node_loads()]
+def local_elastic_stiffness_matrix(E: float, nu: float, A: float, L: float, Iy: float, Iz: float, J: float) -> np.ndarray:
+    k_e = np.zeros((12, 12))
+    # Axial terms - extension of local x axis
+    axial_stiffness = E * A / L
+    k_e[0, 0] = axial_stiffness
+    k_e[0, 6] = -axial_stiffness
+    k_e[6, 0] = -axial_stiffness
+    k_e[6, 6] = axial_stiffness
+    # Torsion terms - rotation about local x axis
+    torsional_stiffness = E * J / (2.0 * (1 + nu) * L)
+    k_e[3, 3] = torsional_stiffness
+    k_e[3, 9] = -torsional_stiffness
+    k_e[9, 3] = -torsional_stiffness
+    k_e[9, 9] = torsional_stiffness
+    # Bending terms - bending about local z axis
+    k_e[1, 1] = E * 12.0 * Iz / L ** 3.0
+    k_e[1, 7] = E * -12.0 * Iz / L ** 3.0
+    k_e[7, 1] = E * -12.0 * Iz / L ** 3.0
+    k_e[7, 7] = E * 12.0 * Iz / L ** 3.0
+    k_e[1, 5] = E * 6.0 * Iz / L ** 2.0
+    k_e[5, 1] = E * 6.0 * Iz / L ** 2.0
+    k_e[1, 11] = E * 6.0 * Iz / L ** 2.0
+    k_e[11, 1] = E * 6.0 * Iz / L ** 2.0
+    k_e[5, 7] = E * -6.0 * Iz / L ** 2.0
+    k_e[7, 5] = E * -6.0 * Iz / L ** 2.0
+    k_e[7, 11] = E * -6.0 * Iz / L ** 2.0
+    k_e[11, 7] = E * -6.0 * Iz / L ** 2.0
+    k_e[5, 5] = E * 4.0 * Iz / L
+    k_e[11, 11] = E * 4.0 * Iz / L
+    k_e[5, 11] = E * 2.0 * Iz / L
+    k_e[11, 5] = E * 2.0 * Iz / L
+    # Bending terms - bending about local y axis
+    k_e[2, 2] = E * 12.0 * Iy / L ** 3.0
+    k_e[2, 8] = E * -12.0 * Iy / L ** 3.0
+    k_e[8, 2] = E * -12.0 * Iy / L ** 3.0
+    k_e[8, 8] = E * 12.0 * Iy / L ** 3.0
+    k_e[2, 4] = E * -6.0 * Iy / L ** 2.0
+    k_e[4, 2] = E * -6.0 * Iy / L ** 2.0
+    k_e[2, 10] = E * -6.0 * Iy / L ** 2.0
+    k_e[10, 2] = E * -6.0 * Iy / L ** 2.0
+    k_e[4, 8] = E * 6.0 * Iy / L ** 2.0
+    k_e[8, 4] = E * 6.0 * Iy / L ** 2.0
+    k_e[8, 10] = E * 6.0 * Iy / L ** 2.0
+    k_e[10, 8] = E * 6.0 * Iy / L ** 2.0
+    k_e[4, 4] = E * 4.0 * Iy / L
+    k_e[10, 10] = E * 4.0 * Iy / L
+    k_e[4, 10] = E * 2.0 * Iy / L
+    k_e[10, 4] = E * 2.0 * Iy / L
+    return k_e
 
-def stiffness_matrix(F, k, E, A, L, u):
+
+def rotation_matrix(x1: float, y1: float, z1: float, x2: float, y2: float, z2: float, z: np.ndarray = None):
+    L = np.sqrt((x2 - x1) ** 2.0 + (y2 - y1) ** 2.0 + (z2 - z1) ** 2.0)
+    lxp = (x2 - x1) / L
+    mxp = (y2 - y1) / L
+    nxp = (z2 - z1) / L
+    local_x = np.asarray([lxp, mxp, nxp])
+
+    v_temp = z
+    # choose a vector to make orthogonal relative to the y axis if one is not given
+    if v_temp is None:
+        if np.isclose(lxp, 0.0) and np.isclose(mxp, 0.0):
+            v_temp = np.array([0, 1.0, 0.0])
+        else:
+            # otherwise use the global z axis
+            v_temp = np.array([0, 0, 1.0])
+    else:
+        # check to make sure that given v_temp is a unit vector
+        check_unit_vector(v_temp)
+        # check to make sure that given v_temp is not parallel to the local x axis
+        check_parallel(local_x, v_temp)
     
+    local_y = np.cross(v_temp, local_x)
+    local_y = local_y / np.linalg.norm(local_y)
+    local_z = np.cross(local_x, local_y)
+    local_z = local_z / np.linalg.norm(local_z)
+    gamma = np.vstack((local_x, local_y, local_z))
     
-    
+    return gamma
+
+
+def transformation_matrix(gamma: np.ndarray) -> np.ndarray:
+    Gamma = np.zeros((12, 12))
+    Gamma[0:3, 0:3] = gamma
+    Gamma[3:6, 3:6] = gamma
+    Gamma[6:9, 6:9] = gamma
+    Gamma[9:12, 9:12] = gamma
+    return Gamma
+
+
+def local_geometric_stiffness_matrix(L, A, I_rho, Fx2, Mx2, My1, Mz1, My2, Mz2):
+    k_g = np.zeros((12, 12))
+    # upper triangle off diagonal terms
+    k_g[0, 6] = -Fx2 / L
+    k_g[1, 3] = My1 / L
+    k_g[1, 4] = Mx2 / L
+    k_g[1, 5] = Fx2 / 10.0
+    k_g[1, 7] = -6.0 * Fx2 / (5.0 * L)
+    k_g[1, 9] = My2 / L
+    k_g[1, 10] = -Mx2 / L
+    k_g[1, 11] = Fx2 / 10.0
+    k_g[2, 3] = Mz1 / L
+    k_g[2, 4] = -Fx2 / 10.0
+    k_g[2, 5] = Mx2 / L
+    k_g[2, 8] = -6.0 * Fx2 / (5.0 * L)
+    k_g[2, 9] = Mz2 / L
+    k_g[2, 10] = -Fx2 / 10.0
+    k_g[2, 11] = -Mx2 / L
+    k_g[3, 4] = -1.0 * (2.0 * Mz1 - Mz2) / 6.0
+    k_g[3, 5] = (2.0 * My1 - My2) / 6.0
+    k_g[3, 7] = -My1 / L
+    k_g[3, 8] = -Mz1 / L
+    k_g[3, 9] = -Fx2 * I_rho / (A * L)
+    k_g[3, 10] = -1.0 * (Mz1 + Mz2) / 6.0
+    k_g[3, 11] = (My1 + My2) / 6.0
+    k_g[4, 7] = -Mx2 / L
+    k_g[4, 8] = Fx2 / 10.0
+    k_g[4, 9] = -1.0 * (Mz1 + Mz2) / 6.0
+    k_g[4, 10] = -Fx2 * L / 30.0
+    k_g[4, 11] = Mx2 / 2.0
+    k_g[5, 7] = -Fx2 / 10.0
+    k_g[5, 8] = -Mx2 / L
+    k_g[5, 9] = (My1 + My2) / 6.0
+    k_g[5, 10] = -Mx2 / 2.0
+    k_g[5, 11] = -Fx2 * L / 30.0
+    k_g[7, 9] = -My2 / L
+    k_g[7, 10] = Mx2 / L
+    k_g[7, 11] = -Fx2 / 10.0
+    k_g[8, 9] = -Mz2 / L
+    k_g[8, 10] = Fx2 / 10.0
+    k_g[8, 11] = Mx2 / L
+    k_g[9, 10] = (Mz1 - 2.0 * Mz2) / 6.0
+    k_g[9, 11] = -1.0 * (My1 - 2.0 * My2) / 6.0
+    # add in the symmetric lower triangle
+    k_g = k_g + k_g.transpose()
+    # add diagonal terms
+    k_g[0, 0] = Fx2 / L
+    k_g[1, 1] = 6.0 * Fx2 / (5.0 * L)
+    k_g[2, 2] = 6.0 * Fx2 / (5.0 * L)
+    k_g[3, 3] = Fx2 * I_rho / (A * L)
+    k_g[4, 4] = 2.0 * Fx2 * L / 15.0
+    k_g[5, 5] = 2.0 * Fx2 * L / 15.0
+    k_g[6, 6] = Fx2 / L
+    k_g[7, 7] = 6.0 * Fx2 / (5.0 * L)
+    k_g[8, 8] = 6.0 * Fx2 / (5.0 * L)
+    k_g[9, 9] = Fx2 * I_rho / (A * L)
+    k_g[10, 10] = 2.0 * Fx2 * L / 15.0
+    k_g[11, 11] = 2.0 * Fx2 * L / 15.0
+    return k_g
+
+
+def local_geometric_stiffness_matrix_3D_beam_without_interaction_terms(L, A, I_rho, Fx2):
+    k_g = np.zeros((12, 12))
+    # upper triangle off diagonal terms
+    k_g[0, 6] = -Fx2 / L
+    k_g[1, 5] = Fx2 / 10.0
+    k_g[1, 7] = -6.0 * Fx2 / (5.0 * L)
+    k_g[1, 11] = Fx2 / 10.0
+    k_g[2, 4] = -Fx2 / 10.0
+    k_g[2, 8] = -6.0 * Fx2 / (5.0 * L)
+    k_g[2, 10] = -Fx2 / 10.0
+    k_g[3, 9] = -Fx2 * I_rho / (A * L)
+    k_g[4, 8] = Fx2 / 10.0
+    k_g[4, 10] = -Fx2 * L / 30.0
+    k_g[5, 7] = -Fx2 / 10
+    k_g[5, 11] = -Fx2 * L / 30.0
+    k_g[7, 11] = -Fx2 / 10.0
+    k_g[8, 10] = Fx2 / 10.0
+    # add in the symmetric lower triangle
+    k_g = k_g + k_g.transpose()
+    # add diagonal terms
+    k_g[0, 0] = Fx2 / L
+    k_g[1, 1] = 6.0 * Fx2 / (5.0 * L)
+    k_g[2, 2] = 6.0 * Fx2 / (5.0 * L)
+    k_g[3, 3] = Fx2 * I_rho / (A * L)
+    k_g[4, 4] = 2.0 * Fx2 * L / 15.0
+    k_g[5, 5] = 2.0 * Fx2 * L / 15.0
+    k_g[6, 6] = Fx2 / L
+    k_g[7, 7] = 6.0 * Fx2 / (5.0 * L)
+    k_g[8, 8] = 6.0 * Fx2 / (5.0 * L)
+    k_g[9, 9] = Fx2 * I_rho / (A * L)
+    k_g[10, 10] = 2.0 * Fx2 * L / 15.0
+    k_g[11, 11] = 2.0 * Fx2 * L / 15.0
+    return k_g
